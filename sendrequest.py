d659ccb52852b90058eb123b9bcb6ba3c777d56e
ishan412 <113770196+ishan412@users.noreply.github.com>
2023-07-04 14:42:35 +0100

    Update sendrequest.py

--- sendrequest.py
+++ sendrequest.py
@@ -10,32 +10,47 @@ auto_scaling_group_name = 'Raft'
 
 # Retrieve the private IP addresses of the instances in the Auto Scaling group
 response = autoscaling_client.describe_auto_scaling_groups(AutoScalingGroupNames=[auto_scaling_group_name])
-instances = response['AutoScalingGroups'][0]['Instances']
-private_ips = [instance['PrivateIpAddress'] for instance in instances]
 
-# Retrieve the status of each Raft node
-statuses = []
-for private_ip in private_ips:
-    url = f"http://{private_ip}:5000/status/raft"
-    response = requests.get(url)
-    status = response.json()
-    statuses.append(status)
+# Check if Auto Scaling group exists
+if 'AutoScalingGroups' in response and len(response['AutoScalingGroups']) > 0:
+    instances = response['AutoScalingGroups'][0]['Instances']
 
-# Find the leader node
-leader_node = None
-for status in statuses:
-    if status["is_current_node_leader"]:
-        leader_node = status["self"]
-        break
+    # Retrieve the private IP addresses of the instances
+    private_ips = []
+    for instance in instances:
+        instance_id = instance['InstanceId']
+        response = ec2_client.describe_instances(InstanceIds=[instance_id])
+        instance_data = response['Reservations'][0]['Instances'][0]
+        private_ip = instance_data['PrivateIpAddress']
+        private_ips.append(private_ip)
 
-if leader_node:
-    # Send a POST request to the leader node
-    url = f"http://{leader_node}:5000/tasks"
-    response = requests.post(url, json={"token": 2})
-    if response.status_code == 200:
-        print("Task sent successfully to the leader node.")
+    # Retrieve the status of each Raft node
+    statuses = []
+    for private_ip in private_ips:
+        url = f"http://{private_ip}:5000/status/raft"
+        response = requests.get(url)
+        status = response.json()
+        statuses.append(status)
+
+    # Find the leader node
+    leader_node = None
+    for status in statuses:
+        if status["is_current_node_leader"]:
+            leader_node = status["self"]
+            break
+
+    if leader_node:
+        # Extract IP address from leader_node
+        leader_ip = leader_node.split(':')[0]
+
+        # Send a POST request to the leader node
+        url = f"http://{leader_ip}:5000/tasks"
+        response = requests.post(url, json={"token": 2})
+        if response.status_code == 200:
+            print("Task sent successfully to the leader node.")
+        else:
+            print("Failed to send the task to the leader node.")
     else:
-        print("Failed to send the task to the leader node.")
+        print("No leader node found.")
 else:
-    print("No leader node found.")
-
+    print("Auto Scaling group not found.")
